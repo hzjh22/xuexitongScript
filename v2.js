243215f7de823a93285179eb3955fcead03beb0e
Pizuk <pizuk8315@gmail.com>
2025-12-19 17:29:43 +0800

    增加了跳过章节测验的功能（需要提前完成章节测验）

--- v2.js
+++ v2.js
@@ -36,7 +36,9 @@
                 this._initCellData();
                 this._videoEl = null;
                 this._getVideoEl();
+                this.play();
             },
+
             /// 选择并播放下一小节视频（需要先调用run方法初始化数据）
             nextUnit() {
                 const el = this._getTreeContainer();
@@ -68,6 +70,17 @@
             async play() {
                 try {
                     const el = this._getVideoEl();
+                    if (el == null && document.getElementsByClassName("prev_title")[0].title === "章节测验") {
+                        // 没找到视频元素
+                        console.log("===========跳过章节测验，2秒后继续播放==============")
+                        $("#prevNextFocusNext").click()
+                        setTimeout(() => {
+                            this.play();
+                        }, 2000);
+                        return;
+                    }
+                    /// 挂钩子以便循环播放
+                    this._videoEventHandle();
                     /// 设置倍数，并播放
                     el.playbackRate = this.configs.playbackRate;
                     await el.play();
@@ -157,10 +170,9 @@
                 if (!this._videoEl) {
                     const frameObj = $("iframe").eq(0).contents().find("iframe.ans-insertvideo-online");
                     if (frameObj.length === 0) {
-                        throw new Error("找不到视频播放区域iframe")
+                        return null;
                     }
                     this._videoEl = frameObj.contents().eq(0).find("video#video_html5_api").get(0);
-                    this._videoEventHandle();
                 }
                 if (!this._videoEl) {
                     throw new Error("视频组件Video未加载完成")
